c483936b19b5e3a79f44a4fa3d5c64c7eac26da3
lai2-24 <lai2-24@itismeucci.com>
2024-10-04 08:48:39 +0200

    aggiunto cestino

---
+++ src/main/java/com/example/Cestino.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Cestino {
+    private int contatore;
+
+    public int getCestino(){
+        return contatore;
+    }
+
+    public Cestino(){
+        
+    }
+}
